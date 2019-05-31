908f0cb63ff375a4abb86a16c6801cb1277a42ed
david serrano <david.serrano@bridgeinternationalacademies.com>
2019-05-31 08:48:37 +0100

    update to 2019.1.4f start building out mouse interactions

--- Assets/Scripts/Mouse.cs
+++ Assets/Scripts/Mouse.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class MousePoint : MonoBehaviour
 {
     RaycastHit hit;
-    int rayLength = 500;
     public GameObject target;
+    public static GameObject currentlySelectedTarget;
+    private Vector3 mouseDownPoint;
+
     // Start is called before the first frame update
     void Start()
     {
+    }
+
+    private void Awake()
+    {
+        mouseDownPoint = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out hit, rayLength))
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
+            if (Input.GetMouseButtonDown(0))
+            {
+                mouseDownPoint = hit.point;
+            }
+
             Debug.Log("Hit: " + hit.collider.name + "x: " + hit.point.x + " y: " + hit.point.y + " z: " + hit.point.z);
             switch (hit.collider.name)
             {
@@ -30,19 +42,77 @@ public class MousePoint : MonoBehaviour
                         GameObject targetObject = Instantiate(target, hit.point, Quaternion.identity) as GameObject;
                         targetObject.name = "Target Instantiated";
                     }
+                    else if (Input.GetMouseButtonUp(0) && DidUserClickLeftMouse(mouseDownPoint))
+                    {
+                        DeselectGameObjectIfSelected();
+                    }
 
                     break;
-                case "Character":
-                    if (Input.GetMouseButtonDown(1)) {
+                default:
+                    if(Input.GetMouseButtonUp(0) && DidUserClickLeftMouse(mouseDownPoint)) {
+                        if(hit.collider.transform.Find("Selected"))
+                        {
+                            Debug.Log("Found a unit!");
+
+                            if (currentlySelectedTarget != hit.collider.gameObject)
+                            {
+                                GameObject selectedObject = hit.collider.transform.Find("Selected").gameObject;
+                                selectedObject.SetActive(true);
+
+                                if(currentlySelectedTarget != null)
+                                {
+                                    currentlySelectedTarget.transform.Find("Selected").gameObject.SetActive(false);
+                                }
+
+                                currentlySelectedTarget = hit.collider.gameObject;
+                            }
+                        } else
+                        {
+                            DeselectGameObjectIfSelected();
+                        }
+
+                    }
+                   /* if (Input.GetMouseButtonDown(1))
+                    {
                         Projector selected = hit.collider.gameObject.GetComponentInChildren<Projector>();
                         selected.enabled = true;
-                    }
-                        break;
+                    }*/
+                    break;
             }
-         
+        }
+        else
+        {
+            if (Input.GetMouseButtonUp(0) && DidUserClickLeftMouse(mouseDownPoint)) {
+                DeselectGameObjectIfSelected();
+            }
+        
         }
 
-        Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.yellow);
+        Debug.DrawRay(ray.origin, ray.direction * Mathf.Infinity, Color.yellow);
 
+    }
+
+    public bool DidUserClickLeftMouse(Vector3 hitPoint)
+    {
+        float clickZone = 1.3f;
+
+        if (
+            (mouseDownPoint.x < hitPoint.x + clickZone && mouseDownPoint.x > hitPoint.x - clickZone) &&
+            (mouseDownPoint.y < hitPoint.y + clickZone && mouseDownPoint.y > hitPoint.y - clickZone) &&
+            (mouseDownPoint.z < hitPoint.z + clickZone && mouseDownPoint.z > hitPoint.z - clickZone)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void DeselectGameObjectIfSelected()
+    {
+        if(currentlySelectedTarget != null)
+        {
+            currentlySelectedTarget.transform.Find("Selected").gameObject.SetActive(false);
+            currentlySelectedTarget = null;
+        }
     }
 }
